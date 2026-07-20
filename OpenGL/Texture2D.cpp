b8197539a532d77e70b2731fe1040c8772bad2e6
Kai <Kaiboucher2007@gmail.com>
2026-07-20 22:23:24 +0100

    T10 Task 5

--- OpenGL/Texture2D.cpp
+++ OpenGL/Texture2D.cpp
@@ -31,6 +31,13 @@ bool Texture2D::Load(char* path, int width, int height)
 	file.read(reinterpret_cast<char*>(data), _width * _height * 3);
 	file.close();
 
+	Upload(data);
+
+	return true;
+}
+
+void Texture2D::Upload(unsigned char* data)
+{
 	glGenTextures(1, &_ID);
 	glBindTexture(GL_TEXTURE_2D, _ID);
 
@@ -42,6 +49,71 @@ bool Texture2D::Load(char* path, int width, int height)
 	gluBuild2DMipmaps(GL_TEXTURE_2D, 3, _width, _height, GL_RGB, GL_UNSIGNED_BYTE, data);
 
 	delete[] data;
+}
+
+bool Texture2D::LoadBMP(char* path)
+{
+	ifstream file(path, ios::binary);
+	if (!file.is_open())
+	{
+		cout << "Failed to open BMP file: " << path << endl;
+		return false;
+	}
+
+	unsigned short bfType;
+	unsigned int bfOffBits;
+	unsigned int biWidth, biHeight;
+	unsigned short biBitCount;
+
+	file.read(reinterpret_cast<char*>(&bfType), 2);
+	if (bfType != 0x4D42)
+	{
+		cout << "Not a valid BMP file: " << path << endl;
+		file.close();
+		return false;
+	}
+
+	file.seekg(10);
+	file.read(reinterpret_cast<char*>(&bfOffBits), 4);
+
+	file.seekg(18);
+	file.read(reinterpret_cast<char*>(&biWidth), 4);
+	file.read(reinterpret_cast<char*>(&biHeight), 4);
+
+	file.seekg(28);
+	file.read(reinterpret_cast<char*>(&biBitCount), 2);
+
+	_width = biWidth;
+	_height = biHeight;
+
+	int channels = biBitCount / 8;
+	int rowSize = ((_width * channels) + 3) & ~3;
+	int dataSize = rowSize * _height;
+
+	unsigned char* bmpData = new unsigned char[dataSize];
+	file.seekg(bfOffBits);
+	file.read(reinterpret_cast<char*>(bmpData), dataSize);
+	file.close();
+
+	unsigned char* rgbData = new unsigned char[_width * _height * 3];
+
+	for (int y = 0; y < _height; y++)
+	{
+		for (int x = 0; x < _width; x++)
+		{
+			int srcRow = (_height - 1 - y) * rowSize;
+			int srcOffset = srcRow + x * 3;
+			int dstOffset = (y * _width + x) * 3;
+
+			rgbData[dstOffset + 0] = bmpData[srcOffset + 2];
+			rgbData[dstOffset + 1] = bmpData[srcOffset + 1];
+			rgbData[dstOffset + 2] = bmpData[srcOffset + 0];
+		}
+	}
+
+	delete[] bmpData;
+
+	Upload(rgbData);
 
 	return true;
 }
--- OpenGL/Texture2D.h
+++ OpenGL/Texture2D.h
@@ -7,11 +7,13 @@ public:
 	Texture2D();
 	~Texture2D();
 	bool Load(char* path, int width, int height);
+	bool LoadBMP(char* path);
 	GLuint GetID() const { return _ID; }
 	int GetWidth() const { return _width; }
 	int GetHeight() const { return _height; }
 
 private:
+	void Upload(unsigned char* data);
 	GLuint _ID;
 	int _width, _height;
 };
